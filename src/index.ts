2083f5be1eb2a88ed1ca916f9548e9261f84aafc
caioagiani <agianicaio@gmail.com>
2021-01-09 22:00:57 -0300

    feat: added new command

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { client } from './server';
-import { EconomyCommand, QuoteCommand, CepCommand } from './app/commands';
+import {
+  EconomyCommand,
+  QuoteCommand,
+  CepCommand,
+  ProfileCommand,
+} from './app/commands';
 import CommandDispatcher from './app/utils/CommandDispatcher';
-import { IMessage } from './app/interfaces/Message';
+import type { Message } from 'whatsapp-web.js';
 
 const dispatcher = new CommandDispatcher();
 
-client.on('message', async (message: IMessage) => {
+client.on('message', async (message: Message) => {
   dispatcher.register('cotacao', new EconomyCommand());
   dispatcher.register('mencionar', new QuoteCommand());
   dispatcher.register('cep', new CepCommand(message.body));
+  dispatcher.register('perfil', new ProfileCommand(message.body));
 
   if (message.body.startsWith('!')) {
     dispatcher.dispatch(message.body.slice(1), message);
