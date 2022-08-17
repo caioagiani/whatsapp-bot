429fc202cdb0b0ba1be972d6552fb867d354914f
Caio Agiani <agianicaio@gmail.com>
2022-08-17 00:55:46 -0300

    feat(project): refact dispatch command (#92)

--- src/services/whatsapp.ts
+++ src/services/whatsapp.ts
@@ -1,8 +1,12 @@
 import { Client, MessageMedia, LocalAuth } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
+import { resolve } from 'path';
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth({
+    clientId: 'wpp-bot',
+    dataPath: resolve(__dirname, '..', 'data'),
+  }),
   puppeteer: {
     headless: true,
     args: ['--no-sandbox'],
@@ -10,13 +14,13 @@ const client = new Client({
 });
 
 client.on('qr', async (qr) => qrcode.generate(qr, { small: true }));
-client.on('authenticated', () => console.log('authenticated'));
-client.on('auth_failure', () => console.log('Authentication failed.'));
+client.on('authenticated', () => console.log('WhatsApp authenticated.'));
+client.on('auth_failure', () => console.log('WhatsApp authentication failed.'));
 client.on('disconnected', () => console.log('WhatsApp lost connection.'));
 client.on('ready', async () => {
   await client.sendMessage(
     '5511999865802@c.us',
-    `[${client.info.pushname}] - WhatsApp Online\n[x] Please, *like* project: https://github.com/caioagiani/whatsapp-bot`,
+    `[${client.info.pushname}] - WhatsApp Online\n\n[⭐] Please *like* this project: https://github.com/caioagiani/whatsapp-bot\n\n[💝] Sponsor this SourceCode: https://github.com/sponsors/caioagiani`,
   );
 
   console.log('WhatsApp bot successfully connected!');
