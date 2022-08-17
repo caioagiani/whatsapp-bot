10a503564ed38be1a5a22a80efb597c0cd75eb06
Caio Agiani <agianicaio@gmail.com>
2022-08-17 00:55:46 -0300

    feat(project): refact dispatch command (#92)

--- src/app/utils/CommandDispatcher.ts
+++ src/app/utils/CommandDispatcher.ts
@@ -1,22 +1,15 @@
 import Command from './Command';
 
 class CommandDispatcher {
-  private commandsHandlers: Map<string, Command<any>> = new Map();
+  private commandsHandlers: Map<string, Command<string>> = new Map();
 
-  async register(name: string, command: Command<any>) {
+  async register(name: string, command: Command<string>) {
     this.commandsHandlers.set(name, command);
   }
 
-  async dispatch(name: string, message: any) {
-    for (const [key, _] of this.commandsHandlers) {
-      if (name.includes(key)) {
-        if (!this.commandsHandlers.has(key)) {
-          return;
-        }
-
-        const command = this.commandsHandlers.get(key);
-        await command.execute(message);
-      }
+  async dispatch(name: string, message: string) {
+    for (const [key, command] of this.commandsHandlers) {
+      if (name.includes(key)) command.execute(message);
     }
   }
 }
